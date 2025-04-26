a5c631ac162ae4ec3e22527e941662eefb3911e0
DrillDev6 <adriel.hipollitoo@gmail.com>
2025-04-26 15:09:22 -0300

    Implementing Authentication & CRUD Users

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    #parametros de login id(int), username(string), password(string), email(string)
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+
+    
